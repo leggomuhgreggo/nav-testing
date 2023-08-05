c1b22147c57d4cb3de3a38d19fface4ceea3aafa
Greg Westneat <greg.westneat@gmail.com>
2023-08-05 13:46:12 -0700

    add react-navigation-app

---
+++ apps/react-navigation/example-app/metro-v71.config.js
@@ -0,0 +1,44 @@
+/**
+ * Old custom configuration for React Native v0.71.
+ * From @react-native/metro-config 0.72.1, it is no longer necessary to use a config function to access the complete default config.
+ * Please port your custom configuration to metro.config.js.
+ * Please see https://docs.expo.dev/guides/customizing-metro/ to learn about configuration.
+ */
+
+const { withNxMetro } = require('@nx/expo');
+const { getDefaultConfig } = require('@expo/metro-config');
+const { mergeConfig } = require('metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const customConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+    blockList: exclusionList([/^(?!.*node_modules).*\/dist\/.*/]),
+    unstable_enableSymlinks: true,
+    unstable_enablePackageExports: true,
+  },
+};
+
+
+module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
+  // Change this to true to see debugging info.
+  // Useful if you have issues resolving modules
+  debug: false,
+  // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
+  extensions: [],
+  // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
+  watchFolders: [],
+});
